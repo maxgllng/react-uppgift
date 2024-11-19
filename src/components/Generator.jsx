53a8314ffeaa86b8664496af94ecd3aa543a1523
maxgllng <max.gylling@utb.ecutbildning.se>
2024-11-19 20:51:52 +0100

    Generator-komponent och design skapad

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -5,7 +5,9 @@ export default function Generator() {
     return (
         <main>
             <div className="generator">
-                <img src={Smiley} />
+                <img src={Smiley} alt="happyface" className="smiley" />
+                <p className="affirmation">You are doing a great job.</p>
+                <button className="generate--btn">Generate</button>
             </div>
         </main>
     )
