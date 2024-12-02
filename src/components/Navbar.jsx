15ae5d67edc406a55f5103489508611ee13bf5c0
maxgllng <max.gylling@utb.ecutbildning.se>
2024-12-02 12:58:28 +0100

    Ändrat navlink i navbar, route och styling på knapp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, NavLink, useLocation } from 'react-router-dom';
 import '../styles/navbar.css';
 
+
 export default function Navbar() {
+    const location = useLocation();
+    const [buttonText, setButtonText] = useState('SOS');
+    const [buttonRoute, setButtonRoute] = useState('/Networks');
+
+    useEffect(() => {
+        if (location.pathname === "/Networks") {
+            setButtonText("Return");
+            setButtonRoute("/");
+        } else {
+            setButtonText("SOS");
+            setButtonRoute("/Networks");
+        }
+    }, [location]);
+
     return (
         <nav className="navbar">
             <NavLink to="/" className="logo">PureThoughts.</NavLink>
-            <NavLink to="Networks" className="help--btn">SOS</NavLink>
+            <NavLink
+                to={buttonRoute}
+                className={`help--btn ${buttonText === "Return" ? "back-btn" : ""}`}
+            >
+                {buttonText}
+            </NavLink>
         </nav>
-    )
+    );
 }
